4c264a8a5cb4f33977f8b224b9f3265e0581d37a
Tanishq Sharma <Tanishqsharma.sharma@gmail.com>
2026-05-16 13:48:53 +0530

    Add: concatenation-of-array - submission-0

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-0.js
@@ -0,0 +1,13 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    getConcatenation(nums) {
+        
+       
+        return  nums.concat(nums);
+        
+      
+    }
+}
